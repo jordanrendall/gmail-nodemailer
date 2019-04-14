700bf6071778236b587787e9e7425e202da10956
jordanrendall <thomasjrendall@gmail.com>
2019-04-14 11:21:43 -0400

    Updated app to use command line parameter input for email construction

--- app.js
+++ app.js
@@ -12,6 +12,20 @@ const clientSecret = process.env.CLIENT_SECRET;
 const redirectUrl = process.env.REDIRECT_URL;
 const refreshToken = process.env.REFRESH_TOKEN;
 
+const args = require('minimist')(process.argv.slice(2));
+
+if (!args.use) {
+  return console.log('No --use parameter given');
+}
+
+const {
+  emailFancyFrom,
+  emailList,
+  emailSubject,
+  emailPlainText,
+  emailHTML,
+} = require(`./local/${args.use}`);
+
 //Create OAuth2 client
 const oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
 
@@ -32,16 +46,6 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-//Code up fancy email
-const emailFancyFrom = `<enter text here> 😃`;
-let emailList = [`<enter email here>, <enter second email here> ...`];
-const emailSubject = `<enter text here> 😆`;
-const emailPlainText = `<enter text here>`;
-const emailHTML = `
-<h1>Example Email</h1>
-<p>Hello ${emailList[0]}!</p>
-`;
-
 //Create message object based on custom email parameters
 const message = {
   from: `${emailFancyFrom} <${process.env.SENDER_EMAIL}>`,
@@ -54,9 +58,7 @@ const message = {
 //Send the message to the email list
 transporter.sendMail(message, (error, response) => {
   if (error) {
-    console.log(error);
     return;
   }
-  console.log(response);
   transporter.close();
 });
